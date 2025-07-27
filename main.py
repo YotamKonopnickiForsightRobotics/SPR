6203a6c7001153fe73ea7fc4b5b8fc19f8c7dd1d
YotamKonopnickiForsightRobotics <yotam.konopnicki@forsightrobotics.com>
2025-07-27 15:57:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import os
 import re
 from datetime import datetime
 import zipfile
-import time
+import shutil
+import threading
+import sys
+
 
 # ==============================
 # Configuration Constants
@@ -20,8 +23,10 @@ SSH_PASSWORD = "rubyruby"
 system_mapping = {4: "SYS_A", 5: "SYS_B", 6: "SYS_C", 7: "SYS_D", 8: "SYS_E", 9: "SYS_F", 10: "SYS_G", 11: "SYS_H"}
 
 # Local paths
+LOCAL_DIR_OUTPUT = "SPR_output"
 LOCAL_ZIP_PATH = "SPR"
 LOCAL_DESCRIPTION_FILENAME = "description.txt"
+DRIVE_DIR_PATH = r"G:\Shared drives\ForSight Shared Drive\R&D\Integrations\SPR_LOGS"
 
 # Remote command/file paths
 REMOTE_LAST_VC_PATH_FILE = "/opt/fs_data/last_vc_run_path.txt"
@@ -41,11 +46,13 @@ dirs_to_copy = [
 
 # Files to copy from the remote machine
 files_to_copy = [
-    "aruco_config_json", "Augmentation.stdout.txt", "cataract_tools_oryom_fim_toolset.json",
+    "aruco_config.json", "Augmentations.stdout.txt", "cataract_tools_oryom_fim_toolset.json",
     "Microscope.stdout.txt", "rc_sys_ts_sync.log", "renderer_process.stdout.txt", "RKS.stdout.txt",
     "VisionComputer.global.ini", "VisionComputer.local.ini", "VPP.log"
 ]
 
+def cancel_action():
+    sys.exit()
 
 def center_window(win):
     win.update_idletasks()  # Ensure window dimensions are calculated
@@ -60,7 +67,7 @@ class ProgressWindow:
     def __init__(self, parent):
         self.root = tk.Toplevel(parent)
         self.root.title("Progress")
-        self.root.geometry("400x120")
+        self.root.geometry("400x160")
         self.root.resizable(False, False)
         self.label = tk.Label(self.root, text="Starting...", font=("Segoe UI", 12))
         self.label.pack(pady=(20, 10))
@@ -68,27 +75,34 @@ class ProgressWindow:
         self.progress.pack(pady=(0, 20))
         self.progress["value"] = 0
         self.root.overrideredirect(True)
+        self.cancel = tk.Button(self.root, text="cancel", command=cancel_action, font=("Segoe UI", 10))
+        self.cancel.pack(pady=(10, 0))
         center_window(self.root)
         self.root.update()
 
     def update_status(self, message, value):
-        self.label.config(text=message)
-        self.progress["value"] = value
-        self.root.update()
+        def _update():
+            self.label.config(text=message)
+            self.progress["value"] = value
+
+        self.root.after(0, _update)
 
     def close_after_delay(self, delay=3):
-        time.sleep(delay)
-        self.root.destroy()
+        self.root.after(delay * 1000, self.root.destroy)
+        os._exit(1)
 
 
 
 
 
 
-def get_path_to_last_pcap(client, recordings_path):
+def get_path_to_last_pcap(client, recordings_path, n):
     """
     Extracts and returns paths to the 5 most recent .pcap files from a recordings directory on a remote machine.
     """
+    if not "recordings" in ssh_run_command(client, f"cd {recordings_path[:-10]} && ls"):
+        return []
+
     all_pcaps = ssh_run_command(client, f"cd {recordings_path} && ls")
 
     matches = re.findall(r'([^\s]+?\.pcap)(\d+)', all_pcaps)
@@ -99,13 +113,13 @@ def get_path_to_last_pcap(client, recordings_path):
     # Sort by number descending
     files_with_numbers.sort(key=lambda x: x[1], reverse=True)
 
-    # Take top 5 filenames
-    top_5_files = [name for name, _ in files_with_numbers[:5]]
+    # Take top n filenames
+    top_n_files = [name for name, _ in files_with_numbers[:n]]
 
-    for i in range(0, min(5, len(top_5_files))):
-        top_5_files[i] = f"{recordings_path}/{top_5_files[i]}"
+    for i in range(0, min(n, len(top_n_files))):
+        top_n_files[i] = f"{recordings_path}/{top_n_files[i]}"
 
-    return top_5_files
+    return top_n_files
 
 
 
@@ -146,6 +160,22 @@ def save_and_quit():
     Saves the user-entered description text into description.txt and closes the GUI window.
     """
     text = text_box.get("1.0", tk.END).strip()
+    global short_description
+    short_description += str(short_des_text.get("1.0", tk.END).strip())
+
+    global take_rec
+    take_rec = Take_recordings_button_output.get()
+
+
+    global num_of_pcaps
+    if(str(num_pcaps_txt.get("1.0", tk.END).strip() != "")):
+        try:
+
+            num_of_pcaps = int(num_pcaps_txt.get("1.0", tk.END).strip())
+        except:
+            num_of_pcaps = 5
+
+
     with open(LOCAL_DESCRIPTION_FILENAME, "w", encoding="utf-8") as f:
         f.write(text)
     root.destroy()
@@ -203,6 +233,7 @@ def zip_log_files(progress):
     zips them remotely, copies the ZIP file to local machine, then deletes the remote ZIP.
     """
 
+
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     client.connect(hostname=SSH_HOST, username=SSH_USERNAME, password=SSH_PASSWORD, port=SSH_PORT)
@@ -221,7 +252,7 @@ def zip_log_files(progress):
     zip_path = current_run_dir + "/" + REMOTE_ZIP_NAME
     recordings_path = current_run_dir + "/" + REMOTE_RECORDINGS_SUBDIR
 
-    last_5_pcap_files = get_path_to_last_pcap(client, recordings_path)
+    last_n_pcap_files = get_path_to_last_pcap(client, recordings_path, num_of_pcaps)
 
     files_in_log_dir = ssh_run_command(client, f"cd {current_run_dir} && ls")
 
@@ -237,11 +268,15 @@ def zip_log_files(progress):
         if dir in files_in_log_dir:
             paths_to_dirs_to_copy.append(f"{current_run_dir}/{dir}")
 
+    hevc_path = current_run_dir + "/" + "*.hevc"
 
-    str_of_all_files_to_copy = build_files_string((paths_to_files_to_copy, paths_to_dirs_to_copy, last_5_pcap_files))
+    str_of_all_files_to_copy = build_files_string((paths_to_files_to_copy, paths_to_dirs_to_copy,
+                                                   last_n_pcap_files))
 
     progress.update_status("zipping files", 7)
 
+    if take_rec:
+        str_of_all_files_to_copy += hevc_path
 
     try:
         ssh_run_command(client, f"zip -r {zip_path} {str_of_all_files_to_copy}")
@@ -255,15 +290,23 @@ def zip_log_files(progress):
     except Exception as e:
         raise Exception("invalid system")
 
-    local_zip_path = LOCAL_ZIP_PATH + "_" + get_time_now() + "_" + system + ".zip"
+    now = get_time_now()
+    local_zip_path = LOCAL_DIR_OUTPUT + "/" + LOCAL_ZIP_PATH + "_" + now + "_" + system + "_" + short_description + ".zip"
 
+    os.makedirs(LOCAL_DIR_OUTPUT, exist_ok=True)
     copy_remote_file_to_local(client, zip_path, local_zip_path)
     ssh_run_command(client, f"rm {zip_path}")
-    progress.update_status("finished zipping files", 100)
+    progress.update_status("copying to google drive", 80)
+
+    drive_path = DRIVE_DIR_PATH + "/" + LOCAL_ZIP_PATH + "_" + now + "_" + system + "_" + short_description + ".zip"
+
+    add_description_to_zip(local_zip_path)
+
+    shutil.copy(local_zip_path, drive_path)
+
+    progress.update_status("finished copying", 100)
 
     client.close()
-
-    return local_zip_path
 
 
 def main():
@@ -281,39 +324,67 @@ def main():
 
     zip_path = ""
 
-    try:
-        zip_path = zip_log_files(progress)
-    except Exception as e:
-        progress.update_status(f"there was an error with the zip:\n{e}\nfinish doing it urself", 0)
-        progress.close_after_delay(5)
-        return
+    def threaded_zip():
+        try:
+            zip_log_files(progress)
+            progress.close_after_delay()
+            sys.exit()
 
-    add_description_to_zip(zip_path)
+        except Exception as e:
+            sys.exit()
 
-    progress.close_after_delay()
+    threading.Thread(target=threaded_zip, daemon=True).start()
+
+    progress.root.mainloop()
+
+
+
+
+
 
 
 # Set up GUI window for description
 root = tk.Tk()
 root.title("Enter Description")
-root.geometry("600x400")  # Wider and taller window
+root.geometry("800x600")  # Wider and taller window
 
 # Frame to contain everything
 frame = tk.Frame(root, padx=20, pady=20)
 frame.pack(expand=True, fill="both")
 
+
+tk.Label(frame, text="short description: (this goes in the output file name)", font=("Segoe UI", 12)).pack(anchor="w")
+short_des_text = tk.Text(frame, width= 60, height=1, font=("Segoe UI", 12))
+short_des_text.pack(anchor="w", fill="x")
+
+tk.Label(frame, text="number of pcap files (leave empty = 5 files)", font=("Segoe UI", 12)).pack(anchor="w")
+num_pcaps_txt = tk.Text(frame, width= 60, height=1, font=("Segoe UI", 12))
+num_pcaps_txt.pack(anchor="w", fill="x")
+
 # Label
 tk.Label(frame, text="Enter your description:", font=("Segoe UI", 12)).pack(anchor="w", pady=(0, 5))
 
 # Text box
-text_box = tk.Text(frame, height=12, width=60, font=("Segoe UI", 12))
+text_box = tk.Text(frame, height=10, width=60, font=("Segoe UI", 12))
 text_box.pack(fill="both", expand=True)
+
+Take_recordings_button_output = tk.BooleanVar()
+checkbox = tk.Checkbutton(frame, text="include recordings", variable=Take_recordings_button_output, onvalue=True, offvalue=False, font=("Segoe UI", 12))
+checkbox.pack(anchor="w")
 
 # Save button
 enter_button = tk.Button(frame, text="Save and Close", command=save_and_quit, font=("Segoe UI", 10))
 enter_button.pack(pady=(10, 0))
-center_window(root)
 
+# cancel button
+cancel_button = tk.Button(frame, text="cancel", command=cancel_action, font=("Segoe UI", 10))
+cancel_button.pack(pady=(10, 0))
+
+center_window(root)
+short_description = ""
+
+num_of_pcaps = 5
+take_rec = False
 
 if __name__ == "__main__":
     main()
